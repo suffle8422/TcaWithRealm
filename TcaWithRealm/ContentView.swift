6b38bd17a94846c1fdd2e299ec989ebf9af81195
suffle8422 <inf.sky8422@gmail.com>
2023-09-17 15:23:54 +0900

    Initial Commit

---
+++ TcaWithRealm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TcaWithRealm
+//
+//  Created by Ibuki Ohnishi on 2023/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TcaWithRealm/TcaWithRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TcaWithRealmApp.swift
+//  TcaWithRealm
+//
+//  Created by Ibuki Ohnishi on 2023/09/17.
+//
+
+import SwiftUI
+
+@main
+struct TcaWithRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
